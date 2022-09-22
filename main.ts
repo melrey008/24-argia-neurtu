3793622b689daa65a8e0f5493d637d998b4c7e0c
melrey008 <101406273+melrey008@users.noreply.github.com>
2022-09-22 13:09:55 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,47 @@
+basic.forever(function () {
+    if (input.lightLevel() > 20) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    }
+    if (input.lightLevel() > 30) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (input.lightLevel() > 40) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (input.lightLevel() > 55) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (input.lightLevel() > 75) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
